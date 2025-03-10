1cb5a497692ce1fe147ce508fe24f65e09aab02e
Adam Ginzberg <aginzberg@subframe.com>
2025-03-10 13:11:21 -0700

    Only support gpt-4o-mini

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,11 +1,17 @@
 import {convertToCoreMessages, streamText} from 'ai';
 import { openai } from '@ai-sdk/openai';
+import { MODEL_TO_FRIENDLY_NAME } from '../../model-helpers';
 
 export async function POST(req: Request) {
   const { messages, data: { model } } = await req.json();
 
+  let modelToUse = model;
+  if (!(model in MODEL_TO_FRIENDLY_NAME)) {
+    modelToUse = "gpt-4o-mini";
+  }
+
   const result = await streamText({
-    model: openai(model),
+    model: openai(modelToUse),
     messages: convertToCoreMessages(messages),
   });
 
